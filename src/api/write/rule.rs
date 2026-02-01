faf2906ecd3e1a118cdab40a494c32b5fd926e4e
Jamie Wells <jamiewells528@gmail.com>
2026-02-01 23:04:17 +0000

    tweak

--- src/api/write/rule.rs
+++ src/api/write/rule.rs
@@ -1,6 +1,6 @@
 use crate::err::RulesError;
 
-pub fn write(file_name: &str, rule: String) -> Result<(), RulesError> {
+pub fn write(file_name: &str, rule: &str) -> Result<(), RulesError> {
     // TODO: first validate rule
     Ok(())
 }
